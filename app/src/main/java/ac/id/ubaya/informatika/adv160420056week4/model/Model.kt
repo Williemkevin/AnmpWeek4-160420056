fe1193ad61e5ec942b0620f3650acfc0dac0f045
Williemkevin <101261457+Williemkevin@users.noreply.github.com>
2023-03-24 20:57:38 +0700

    init

---
+++ app/src/main/java/ac/id/ubaya/informatika/adv160420056week4/model/Model.kt
@@ -0,0 +1,9 @@
+package ac.id.ubaya.informatika.adv160420056week4.model
+
+data class Student(
+    val id:String?,
+    val name:String?,
+    val dob:String?,
+    val phone:String?,
+    val photoUrl:String?
+)
---
+++ app/src/main/java/ac/id/ubaya/informatika/adv160420056week4/view/StudentListAdapter.kt
@@ -0,0 +1,42 @@
+package ac.id.ubaya.informatika.adv160420056week4.view
+
+import ac.id.ubaya.informatika.adv160420056week4.R
+import ac.id.ubaya.informatika.adv160420056week4.model.Student
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+
+class StudentListAdapter(val studentList:ArrayList<Student>) : RecyclerView.Adapter<StudentListAdapter.StudentViewHolder>() {
+    class  StudentViewHolder(var view: View) : RecyclerView.ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudentViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.student_list_item, parent, false)
+
+        return StudentViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return studentList.size
+    }
+
+    override fun onBindViewHolder(holder: StudentViewHolder, position: Int) {
+        holder.view.findViewById<TextView>(R.id.txtID).text = studentList[position].id
+        holder.view.findViewById<TextView>(R.id.txtName).text = studentList[position].name
+
+        holder.view.findViewById<Button>(R.id.btnDetail).setOnClickListener {
+            val action = StudentListFragmentDirections.actionStudentDetail()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+
+    fun updateStudentList(newStudentList: ArrayList<Student>){
+        studentList.clear()
+        studentList.addAll(newStudentList)
+        notifyDataSetChanged()
+    }
+}
